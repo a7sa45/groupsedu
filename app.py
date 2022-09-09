d0d52e52f5edb9b38c1dcc214d1c70a49845b52f
ABDULHADI AL-BO-SNINAH <abod72001@gmail.com>
2022-09-09 18:20:03 +0300

    first commit

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from tokenize import group
 from flask import Flask, render_template, request, redirect, url_for, Response
 from werkzeug.exceptions import HTTPException
 from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin, AdminIndexView
-from flask_admin.contrib.sqla import ModelView
+#from flask_admin import Admin, AdminIndexView
+#from flask_admin.contrib.sqla import ModelView
 from webargs import flaskparser, fields
 from flask_basicauth import BasicAuth
 from datetime import datetime
@@ -17,8 +17,8 @@ import os
 
 app = Flask(__name__)
 dir = os.path.dirname(os.path.abspath(__file__))
-#database_file = "sqlite:///{}".format(os.path.join(dir, "groupsedu.db"))
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or 'sqlite:///groupsedu.db'
+database_file = "sqlite:///{}".format(os.path.join(dir, "groupsedu.db"))
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 app.config["SECRET_KEY"] = 'secret'
 db = SQLAlchemy(app)
 
@@ -29,7 +29,7 @@ app.config['BASIC_AUTH_PASSWORD'] = 'Gen://Aa123098.com'
 basic_auth = BasicAuth(app)
 app.config['ADMIN_CREDENTIALS'] = ('a7sa45', 'Gen://Aa123098.com')
 
-app.debug =True
+#app.debug =True
 
 
 
@@ -49,28 +49,45 @@ class Group(db.Model):
 
 
 #admin page
-admin = Admin(app)
-admin.add_view(ModelView(Group, db.session))
+#admin = Admin(app)
+#admin.add_view(ModelView(Group, db.session))
 
 
 #CRUD Achins
-def create(email, universityid, subject, code, university, section, url):
+def create_group(email, universityid, subject, code, university, section, url):
     group = Group(email=email, universityid=universityid, subject=subject, code=code, university=university, section=section, url=url)
     db.session.add(group)
     db.session.commit()
     db.session.refresh(group)
 
+def update_group(group_id, email, universityid, subject, code, university, section, url):
+    #group = Group(email=email, universityid=universityid, subject=subject, code=code, university=university, section=section, url=url)
+    db.session.query(Group).filter_by(id=group_id).update({
+        "email": email,
+        "universityid": universityid,
+        "subject": subject,
+        "code": code,
+        "university": university,
+        "section": section,
+        "url": url
+    })
+    db.session.commit()
+
 def read():
     return db.session.query(Group).all()
+
+def delete_group(group_id):
+    db.session.query(Group).filter_by(id=group_id).delete()
+    db.session.commit()
 
 
 @app.errorhandler(404)
 def not_found(e):
     return render_template("404.html")
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+#@app.before_first_request
+#def create_tables():
+#    db.create_all()
 
 @app.route('/')
 def index():
@@ -80,10 +97,10 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/admins')
+@app.route('/myadmin')
 @basic_auth.required
-def admins():
-    return render_template('admins.html')
+def myadmin():
+    return render_template('myadmin.html', groups = read())
 
 @app.route('/add')
 def add():
@@ -92,9 +109,28 @@ def add():
 @app.route('/add', methods=["POST", "GET"])
 def adds():
     if request.method == "POST":
-        create(request.form['email'], request.form['universityid'], request.form['subject'], request.form['code'], request.form['university'], request.form['section'], request.form['url'])
+        create_group(request.form['email'], request.form['universityid'], request.form['subject'], request.form['code'], request.form['university'], request.form['section'], request.form['url'])
     return redirect(url_for('index'))
 
+@app.route('/delete/<group_id>', methods=["POST", "GET"])
+@basic_auth.required
+def delete(group_id):
+    delete_group(group_id)
+    return redirect(url_for('myadmin'))
+
+@app.route('/edit/<group_id>', methods=["POST", "GET"])
+@basic_auth.required
+def edit(group_id):
+    group = db.session.query(Group).filter_by(id=group_id).one()
+    db.session.commit()
+    return render_template('edit.html', group = group)
+
+@app.route('/update', methods=["POST", "GET"])
+@basic_auth.required
+def update():
+    if request.method == "POST":
+        update_group(request.form['group_id'], request.form['email'], request.form['universityid'], request.form['subject'], request.form['code'], request.form['university'], request.form['section'], request.form['url'])
+    return redirect(url_for('myadmin'))
 
 
 
@@ -103,4 +139,5 @@ def adds():
 
 
 if __name__ == '__main__':
-    app.run()
+    db.create_all()
+    app.run(debug =True)
